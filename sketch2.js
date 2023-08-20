b309344cdf30bf83c17de239836c91e5da3c6b07
arnavkrishna <arnavkrishna.1@gmail.com>
2023-08-20 16:44:56 -0400

    kaleidoscope

--- sketch2.js
+++ sketch2.js
@@ -50,7 +50,7 @@ function draw() {
     stroke(currentColor);  // Setting the color for the kaleidoscope line
 
     // Kaleidoscope drawing
-    translate(width / 2, height / 2 - 38);
+    translate(width / 2, height / 2 );
 
     if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
         let mx = mouseX - width / 2;
